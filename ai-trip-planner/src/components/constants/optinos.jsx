90a12a133ba9fb24687e8f9d5c9017d14d7459ed
Nevil Vasani <nevil.vasani114501@marwadiuniversity.ac.in>
2024-08-06 21:50:45 +0530

    updae create trip page

--- ai-trip-planner/src/components/constants/optinos.jsx
+++ ai-trip-planner/src/components/constants/optinos.jsx
@@ -3,21 +3,28 @@ export const SelectTravelList = [
         id : 1,
         title:'Just me',
         desc:'A sole traveler in exploration',
-        icon:'',
+        icon:'✈️',
         people:'1 people'
     },
     {
         id : 2,
         title:'A Couple',
         desc:'Two travelers in tandem',
-        icon:'',
+        icon:'🥂',
         people:'2 people'
     },
     {
         id : 3,
         title:'Family',
         desc:'A group of fun-loving adventurers',
-        icon:'',
+        icon:'🏡',
+        people:'3 to 5 people'
+    },
+    {
+        id : 4,
+        title:'Friends',
+        desc:'A bunches of thrill-seekes',
+        icon:'🏡',
         people:'3 to 5 people'
     }
 ]
@@ -27,18 +34,18 @@ export const SelectBudgteOptions = [
         id : 1,
         title:'Cheap',
         desc:'Stay conscious of costs',
-        icon:''
+        icon:'💵'
     },
     {
         id : 2,
         title:'Moderate',
         desc:'Keep costs on the average side',
-        icon:''
+        icon:'💰'
     },
     {
         id : 3,
         title:'Luxury',
         desc:"Don't worry about cost",
-        icon:''
+        icon:'💸'
     }
 ]
