9a176acab1a69512003c2dc7252fb60682a53a47
Nevil Vasani <nevil.vasani114501@marwadiuniversity.ac.in>
2024-08-06 09:02:14 +0530

    add firebase

--- ai-trip-planner/src/components/ui/custom/Header.jsx
+++ ai-trip-planner/src/components/ui/custom/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from '../button'
 
 function Header() {
   return (
-    <div>
-      <h1>hello from header</h1>
+    <div className='p-2 shadow-sm flex justify-between items-center px-3'>
+        <img src="/vite.svg" alt="Logo" />
+        <div>
+          <Button>Sign in</Button>
+        </div>
     </div>
   )
 }
--- ai-trip-planner/src/components/ui/custom/Hero.jsx
+++ ai-trip-planner/src/components/ui/custom/Hero.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 function Hero() {
   return (
     <div>
-      <h1>Hero</h1>
+      
+      <div className="container mx-50 text-center py-10">
+          <h1 className="text-7xl font-bold mb-4">Your Next Journey, Optimized</h1>
+          <p className="text-lg font-sans mb-6 mx-60">Build, personalize, and optimize your itineraries with our free AI trip planner. Designed for vacations, workations, and everyday adventures.</p>
+          <button className="bg-secondary text-secondary-foreground hover:bg-secondary/80 px-4 py-2 rounded-lg">Create a new trip</button>
+      </div>
+
+      <div className="flex flex-col items-center py-10">
+        <h2 className="text-3xl font-semibold mb-4">Your AI-Powered Trip</h2>
+        <p className="text-lg text-muted-foreground mb-6">The most optimal</p>
+        <p className="text-base mb-6">
+          Craft your perfect itinerary with Trip Planner AI. Our advanced algorithms take into account your selected explore-sights, dining, and lodging preferences to create the optimal travel plan
+          tailored just for you.
+        </p>
+        <img aria-hidden="true" alt="trip-illustration" src="https://openui.fly.dev/openui/300x200.svg?text=🌴" />
+      </div>
     </div>
   )
 }
